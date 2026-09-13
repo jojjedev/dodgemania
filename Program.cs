c13b938205a57d2032dec7b61e27427e505b5f08
jojjedev <johanholmberg20@gmail.com>
2026-09-13 18:09:21 +0200

    added projectile

--- Program.cs
+++ Program.cs
@@ -6,21 +6,26 @@ namespace smflTest;
 
 public class Program
 {
+    public const int ScreenW = 1200;
+    public const int ScreenH = 800;
     static void Main(string[] args)
     {
         using (var window = new RenderWindow(
-                   new VideoMode(1200, 800), "breakout"))
+                   new VideoMode(ScreenW, ScreenH), "breakout"))
         {
             window.Closed += (o, e) => window.Close();
             Clock clock = new Clock();
+            Projectile projectile = new Projectile();
             while (window.IsOpen)
             {
                 float dt = clock.Restart().AsSeconds();
                 window.DispatchEvents(); // Hanterar alla värdesändringar som har gjorts sedan senaste framen. Ex muspekare flyttats,
                                         // Enemy har dödats eller flyttats på sig etc.
                 // TODO: Update screen
-                window.Clear(new Color(131, 197, 235));
+                projectile.Update(dt);
+                window.Clear(new Color(46, 15, 15));
                 // TODO: Draw elements on screen
+                projectile.Draw(window);
                 window.Display(); // Draw the actual screen with all elements.
             }
         }
--- Projectile.cs
+++ Projectile.cs
@@ -6,7 +6,7 @@ namespace smflTest;
 public class Projectile
 {
     public Sprite sprite;
-    public const float Diameter = 20.0f;
+    public const float Diameter = 100.0f;
     public const float Radius = Diameter * 0.5f;
     public Vector2f Position;
     public Vector2f direction = new Vector2f(1, 1) / MathF.Sqrt(2.0f);
@@ -18,6 +18,73 @@ public class Projectile
     {
         sprite = new Sprite();
         sprite.Texture = new Texture("assets/blueBullet.png");
-        sprite.Position = new Vector2f()
+        sprite.Position = new Vector2f(Program.ScreenH / 2, Program.ScreenW / 2);
+        Vector2f projectileTextureSize = (Vector2f)sprite.Texture.Size;
+        sprite.Origin = 0.5f * projectileTextureSize;
+        sprite.Scale = new Vector2f(
+            Diameter / projectileTextureSize.Y,
+            Diameter / projectileTextureSize.Y);
+        gui = new Text();
+        gui.CharacterSize = 30;
+        gui.Font = new Font("assets/vcr.ttf");
+        gui.FillColor = new Color(168, 3, 3);
+
+    }
+
+    public void Draw(RenderTarget target)
+    {
+        // Draws projectile
+        target.Draw(sprite);
+        
+        // Draws "Health" text
+        gui.DisplayedString =  $"Health: {Health}";
+        gui.Position = new Vector2f(12, 8);
+        target.Draw(gui);
+        
+        // Draws "Score" text
+        gui.DisplayedString = $"Score: {Score}";
+        gui.Position = new Vector2f(Program.ScreenW - gui.GetGlobalBounds().Width - 12, 8);
+        target.Draw(gui);
+        
+    }
+
+    public void Update(float dt)
+    {
+        var newPos = sprite.Position;
+        newPos = direction * dt * 100.0f;
+        sprite.Position = newPos;
+
+    }
+
+    public static Vector2f RandomPosition()
+    {
+        Random random = new Random();
+        int side = random.Next(1, 5);
+        int pos;
+        Vector2f spawn = new Vector2f();
+        switch (side)
+        {
+            case 1: // Left side
+                pos = random.Next(0, Program.ScreenW);
+                spawn = new Vector2f(0, pos);
+                break;
+            case 3: // Right side
+                pos = random.Next(0, Program.ScreenW);
+                spawn = new Vector2f(Program.ScreenW, pos);
+                break;
+                
+            case 2: // Top side
+                pos = random.Next(0, Program.ScreenH);
+                spawn = new Vector2f(pos, 0);
+                break;
+                
+            case 4: // Bottom side
+                pos = random.Next(0, Program.ScreenH);
+                spawn = new Vector2f(pos, Program.ScreenH);
+                break;
+        }
+
+        return spawn;
+
     }
 }
